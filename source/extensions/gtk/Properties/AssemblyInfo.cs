315ce34898aa75afb124e2c3d72dbc67d67ab36f
spiceywolf <spiceywolf.archaicsoft@outlook.com>
2020-12-28 01:56:09 -0500

    Removed GUI extensions.

--- source/extensions/gtk/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("SFML.Portable.Gtk")]
-[assembly: AssemblyDescription("SFML Portable Gtk Extension.")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyVersion("2.5.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyProduct("SFML.Net 2.5")]
--- source/extensions/winforms/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("SFML.Portable.Winforms")]
-[assembly: AssemblyDescription("SFML Portable Winforms Extension.")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyVersion("2.5.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyProduct("SFML.Net 2.5")]
