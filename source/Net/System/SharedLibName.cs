c477a47c53e56819ef24a85477278558cd2bac76
spiceywolf <spiceywolf.archaicsoft@outlook.com>
2021-01-01 17:34:36 -0500

    Potential fix to linux side for windows-naming compatibility

--- source/Net/System/SharedLibName.cs
+++ source/Net/System/SharedLibName.cs
@@ -4,12 +4,12 @@ namespace SFML.System
     public static class CSFML
     {
         /// <summary>CSFML Audio Lib Name</summary>
-        public const string audio = "csfml-audio";
+        public const string audio = "csfml-audio-2";
         /// <summary>CSFML Graphics Lib Name</summary>
-        public const string graphics = "csfml-graphics";
+        public const string graphics = "csfml-graphics-2";
         /// <summary>CSFML System Lib Name</summary>
-        public const string system = "csfml-system";
+        public const string system = "csfml-system-2";
         /// <summary>CSFML Window Lib Name</summary>
-        public const string window = "csfml-window";
+        public const string window = "csfml-window-2";
     }
 }
